e5a811adfbba2148051467d9ece35056ff7080e1
Leah Ewing <leahewing@live.com>
2021-06-01 20:01:22 +0000

    Commit Login Page

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import (Flask,render_template, request, flash, session,
                    redirect)
-from model import connect_to_db
-import crud
+#from model import connect_to_db
+#import crud
 from jinja2 import StrictUndefined
 
 app = Flask(__name__)
@@ -14,10 +14,15 @@ def homepage():
 
     return render_template('homepage.html')
 
+@app.route('/login')
+def login_page():
+    """Display user login page."""
+
+    return render_template('login.html')
 
 #put your app routes here! :D
 
 
 if __name__ == '__main__':
-    connect_to_db(app)
+    #connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)

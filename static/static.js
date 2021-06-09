c213ef4be5c3e31654dadd22d027b8e70ab066f5
Leah Ewing <leahewing@live.com>
2021-06-09 21:20:35 +0000

    Commit links 'all-pickup-locations' and 'all-farms'

--- static/static.js
+++ static/static.js
@@ -1,14 +1,17 @@
 'use strict';
 
-
+// list of all pickup locations in 'all-pickup-locations.html'
 $.get('/static/pickup-locations.json', (res) => {
     for (const pickup of res) {
-        $('#pickup-location-api').append(`<li>${pickup.location_name}</li>`);
+        $('#pickup-location-api').append(`<ol><a href = '/pickup-location-info'>${pickup.location_name}</ol>`);
     }
 });
+// links right now aren't going anywhere, make a route in server.py
 
+// list of all farms in 'all-farms.html'
 $.get('/static/farms.json', (res) => {
     for (const farm of res) {
-        $('#farm-api').append(`<li>${farm.farm_name}</li>`);
+        $('#farm-api').append(`<ol><a href = '/farm-info'>${farm.farm_name}</a></ol>`);
     }
 });
+// links right now aren't going anywhere, make a route in server.py
